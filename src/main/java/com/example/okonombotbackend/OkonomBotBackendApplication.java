3582a256a4f7c7aacb32f72db59ed255f4cb6b9f
KristianWede <kris79s4@stud.kea.dk>
2024-04-22 10:55:40 +0200

    Initial commit

---
+++ src/main/java/com/example/okonombotbackend/OkonomBotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.okonombotbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkonomBotBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OkonomBotBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/okonombotbackend/OkonomBotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.okonombotbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkonomBotBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
