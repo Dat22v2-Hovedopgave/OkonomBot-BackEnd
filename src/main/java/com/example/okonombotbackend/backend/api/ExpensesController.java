d58f34a4ac633ce27e3de6c4e4987971990e2dd4
FerhatBaran <161338704+FerhatBaran@users.noreply.github.com>
2024-05-13 09:28:54 +0200

    added expense

--- src/main/java/com/example/okonombotbackend/backend/api/ExpensesController.java
+++ src/main/java/com/example/okonombotbackend/backend/api/ExpensesController.java
@@ -1,13 +1,18 @@
 package com.example.okonombotbackend.backend.api;
 
-import com.example.okonombotbackend.backend.dto.ExpenseRequest;
+import com.example.okonombotbackend.backend.dto.expense.ExpenseDetailedResponse;
+import com.example.okonombotbackend.backend.dto.expense.ExpenseRequest;
 import com.example.okonombotbackend.backend.entity.Expense;
 import com.example.okonombotbackend.backend.service.ExpensesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@CrossOrigin
 @RestController
 @RequestMapping("/api/expenses")
 public class ExpensesController {
@@ -18,10 +23,25 @@ public class ExpensesController {
     public ResponseEntity createExpense(@RequestBody ExpenseRequest body) {
     return ResponseEntity.ok(expensesService.addExpense(body));
     }
-    
+    @GetMapping("/user/{username}")
+    public List<ExpenseDetailedResponse> getExpensesByUserId(@PathVariable String username) {
+        return expensesService.getExpensesByUsername(username);
+    }
 
-    @GetMapping("/user/{userId}")
-    public List<Expense> getExpensesByUserId(@PathVariable int userId) {
-        return null;
+    @PostMapping("/addExpenses")
+    public ResponseEntity<?> addExpenses(@RequestBody List<ExpenseRequest> body){
+        System.out.println(body);
+        List<Expense> expenses = expensesService.addExpenses(body);
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Successfully added earnings! " + expenses.size() + " expenses added");
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{expenseId}")
+    public ResponseEntity<?> deleteExpense(@PathVariable int expenseId) {
+        expensesService.deleteExpense(expenseId);
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Successfully deleted expense with id " + expenseId);
+        return ResponseEntity.ok(response);
     }
 }
