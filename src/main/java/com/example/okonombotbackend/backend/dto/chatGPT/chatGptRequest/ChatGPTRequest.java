ff2ed968ecf9df6093a33b32e522087c18974578
KristianWede <kris79s4@stud.kea.dk>
2024-05-27 18:01:24 +0200

    Fixed chatgpt behavioural issues

--- src/main/java/com/example/okonombotbackend/backend/dto/chatGPT/chatGptRequest/ChatGPTRequest.java
+++ src/main/java/com/example/okonombotbackend/backend/dto/chatGPT/chatGptRequest/ChatGPTRequest.java
@@ -12,21 +12,22 @@ import java.util.ArrayList;
 public class ChatGPTRequest {
     public String model = "gpt-4o";
     public ArrayList<Message> messages;
-    public int max_tokens = 600;
+    public int max_tokens = 800;
+    double temperature = 0.2;
 
 
     public void addMessage(Message newMessage){
         if(messages == null){
             messages = new ArrayList<>();
-            final String content = "Du er en dansk økonomisk rådgiver" +
-                "Jeg vil sende dig et budget indtægter og udgifter for en almen dansk borger. " +
-                    "Jeg har allerede selv et voerblik over tallene, så jeg vil ikke se dem" +
-                "Et negativt tal er en udgift, og et positivt tal er en indtægt. " +
-                "Tallene er angivet i DKK. " +
-                "Vær realistisk og sammenlign dataene med den gennemsnitlige danske økonomi." +
-                "Giv kort generelle råd om økonomi, hvis er nødvendigt. Giv også personlig økonomisk rådgiving. Giv også  løsninger til dine råd" +
-                    "Anbefal en 50/30/20 løsning" +
-                "Formater i html tags. Den ligger allerede i en <body> og lad være med at start sådan her ```html Maks 2000 tegn inkl. mellemrum";
+            final String content =
+                """ 
+                Du er en dansk økonomisk rådgiver Du får tal som repræsenterer indtægter og udgifter.
+                Et negativt tal er en udgift, og et positivt tal er en indtægt. Tallene er angivet i DKK. 
+                Det eneste jeg kræver er økonomiske anbefalinger. vær realistisk og sammenlign dataene med den gennemsnitlige danske økonomi. 
+                Formatér ikke teksten. Gør svaret så kort og konkret som muligt, list ikke tallene, som du får.
+                Anbefal en 50/30/20 løsning Maks 2000 tokens.
+                """;
+
 
             Message system = new Message("system", content);
             messages.add(system);
